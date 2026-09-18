1f468fd03314608b9b66da7f49474033423c5156
Brook8SK <8brook8sk@gmail.com>
2026-09-18 14:51:31 +0600

    privet

---
+++ index.js
@@ -0,0 +1,12 @@
+let names = ["Kubat","ELdar","Bekten","Akyl"];
+
+names.push("Azamat")
+names.unshift("Aky1l")
+names.pop()
+names.shift()
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index]);
+    
+}
+
+
